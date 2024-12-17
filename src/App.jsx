b8e14fd6436510c72ff95e620056d046b0487357
unknown <martinperez9713@gmail.com>
2024-12-16 22:29:55 -0300

    clase10: eventos y formularios 1

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import NavBar from "./components/NavBar/NavBar"
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer"
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import EjemploContadorEventListener from "./components/Ejemplos/EjemploContadorEventListener"
+import EjemploFormulario from "./components/Ejemplos/EjemploFormulario"
 import './App.css'
 
 function App() {
@@ -15,6 +17,8 @@ function App() {
           <Route path="/" element={<ItemListContainer greeting={"Bienvenidos a mi ecommerce"} />} />
           <Route path="/category/:idCategory" element={<ItemListContainer greeting={"Bienvenidos a mi ecommerce"} />} />
           <Route path="/detail/:idProduct" element={<ItemDetailContainer />} />
+          <Route path="/ejemplos" element={<EjemploContadorEventListener />} />
+          <Route path="/formulario" element={<EjemploFormulario />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/data/data.js
+++ src/data/data.js
@@ -2,55 +2,55 @@ const products = [
   {
     id: "Grt223",
     name: "Remera Extreme",
-    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate at, rem iste quia magnam assumenda. Consequatur voluptas beatae ex dolorum dolor sapiente, ipsa est nobis voluptate, odit odio harum quae.",
+    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non aut repellat cupiditate praesentium incidunt ipsum nostrum. Vitae tenetur libero iure! Culpa sapiente assumenda veniam aperiam fugiat excepturi! Facere, sequi illum.",
     price: 400,
     stock: 10,
-    image: "/img/remera-h.jpg",
+    image: ["/img/remera-h.jpg", "/img/remera-h-2.jpg", "/img/remera-h-3.jpg", "/img/remera-h-4.jpg"],
     category: "remeras"
   },
   {
     id: "Grt224",
     name: "Remera Fitz Roy",
-    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate at, rem iste quia magnam assumenda. Consequatur voluptas beatae ex dolorum dolor sapiente, ipsa est nobis voluptate, odit odio harum quae.",
+    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non aut repellat cupiditate praesentium incidunt ipsum nostrum. Vitae tenetur libero iure! Culpa sapiente assumenda veniam aperiam fugiat excepturi! Facere, sequi illum.",
     price: 420,
     stock: 4,
-    image: "/img/remera-m.jpg",
+    image: ["/img/remera-m.jpg", "/img/remera-m-2.jpg", "/img/remera-m-3.jpg", "/img/remera-m-4.jpg"],
     category: "remeras"
   },
   {
     id: "Hff556",
     name: "Pantalon Dorian",
-    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate at, rem iste quia magnam assumenda. Consequatur voluptas beatae ex dolorum dolor sapiente, ipsa est nobis voluptate, odit odio harum quae.",
+    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non aut repellat cupiditate praesentium incidunt ipsum nostrum. Vitae tenetur libero iure! Culpa sapiente assumenda veniam aperiam fugiat excepturi! Facere, sequi illum.",
     price: 720,
     stock: 5,
-    image: "/img/pantalon-h.jpg",
+    image: ["/img/pantalon-h.jpg", "/img/pantalon-h-2.jpg", "/img/pantalon-h-3.jpg", "/img/pantalon-h-4.jpg"],
     category: "pantalones"
   },
   {
     id: "Hff557",
     name: "Pantalon Ambar",
-    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate at, rem iste quia magnam assumenda. Consequatur voluptas beatae ex dolorum dolor sapiente, ipsa est nobis voluptate, odit odio harum quae.",
+    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non aut repellat cupiditate praesentium incidunt ipsum nostrum. Vitae tenetur libero iure! Culpa sapiente assumenda veniam aperiam fugiat excepturi! Facere, sequi illum.",
     price: 620,
     stock: 17,
-    image: "/img/pantalon-m.jpg",
+    image: ["/img/pantalon-m.jpg", "/img/pantalon-m-2.jpg", "/img/pantalon-m-3.jpg", "/img/pantalon-m-4.jpg"],
     category: "pantalones"
   },
   {
     id: "Wre442",
     name: "Zapatillas Cloudfield",
-    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate at, rem iste quia magnam assumenda. Consequatur voluptas beatae ex dolorum dolor sapiente, ipsa est nobis voluptate, odit odio harum quae.",
+    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non aut repellat cupiditate praesentium incidunt ipsum nostrum. Vitae tenetur libero iure! Culpa sapiente assumenda veniam aperiam fugiat excepturi! Facere, sequi illum.",
     price: 800,
     stock: 2,
-    image: "/img/zapatillas-m.jpg",
+    image: ["/img/zapatillas-m.jpg", "/img/zapatillas-m-2.jpg", "/img/zapatillas-m-3.jpg", "/img/zapatillas-m-4.jpg"],
     category: "zapatillas"
   },
   {
     id: "Wre443",
     name: "Zapatillas Space",
-    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate at, rem iste quia magnam assumenda. Consequatur voluptas beatae ex dolorum dolor sapiente, ipsa est nobis voluptate, odit odio harum quae.",
+    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non aut repellat cupiditate praesentium incidunt ipsum nostrum. Vitae tenetur libero iure! Culpa sapiente assumenda veniam aperiam fugiat excepturi! Facere, sequi illum.",
     price: 820,
     stock: 6,
-    image: "/img/zapatillas-h.jpg",
+    image: ["/img/zapatillas-h.jpg", "/img/zapatillas-h-2.jpg", "/img/zapatillas-h-3.jpg", "/img/zapatillas-h-4.jpg"],
     category: "zapatillas"
   },
 ]
